06bc1f55e5dff71831d74e573c3c684639bfac80
Shan CAO <113851256+caoshansesa@users.noreply.github.com>
2024-05-16 09:34:46 -0400

    Update client.c

--- client.c
+++ client.c
@@ -1,73 +1,159 @@
-#include "mbedtls/ctr_drbg.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/net_sockets.h"
 #include "mbedtls/ssl.h"
+#include <arpa/inet.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/error.h>
+#include <mbedtls/net_sockets.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <thread>
+#include <time.h>
+
+#define NOK -1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define SERVER_PORT 802
+#define SERVER_IP "192.168.10.10"
 
 int main()
 {
-    int ret;
+
+    int sock;
+    struct sockaddr_in server_addr;
+    ssize_t bytes_received;
+    
+    // Create the socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Error creating socket");
+        return -1;
+    }
+    
+    // Set up the server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error connecting to server");
+        close(sock);
+        return -1;
+    }
+
+    char KEY_PATH[] = "../role_based_cert/client.key";
+    char CERT_PATH[] = "../role_based_cert/client.crt";
+    char CA_CERT_PATH[] = "../role_based_cert/ca.crt";
+    mbedtls_ssl_config mbed_ssl_conf_;
+    mbedtls_entropy_context mbed_ssl_entropy_;
+    mbedtls_ctr_drbg_context mbed_ssl_ctr_drbg_;
     mbedtls_net_context server_fd;
-    mbedtls_ssl_context ssl;
-    mbedtls_ssl_config conf;
-    mbedtls_entropy_context entropy;
-    mbedtls_ctr_drbg_context ctr_drbg;
+    // Create a local ssl context
+
+    mbedtls_entropy_init(&mbed_ssl_entropy_);
+    mbedtls_ctr_drbg_init(&mbed_ssl_ctr_drbg_);
+    mbedtls_ssl_config_init(&mbed_ssl_conf_);
+    mbedtls_net_init(&server_fd);
 
     const char *pers = "ssl_client";
-    mbedtls_net_init(&server_fd);
-    mbedtls_ssl_init(&ssl);
-    mbedtls_ssl_config_init(&conf);
-    mbedtls_ctr_drbg_init(&ctr_drbg);
-    mbedtls_entropy_init(&entropy);
+    int ret;
 
-    // Seed the RNG
-    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char *)pers,
-                                     strlen(pers))) != 0)
+    if ((ret = mbedtls_ctr_drbg_seed(&mbed_ssl_ctr_drbg_, mbedtls_entropy_func, &mbed_ssl_entropy_,
+                                     (const unsigned char *)pers, strlen(pers))) != 0)
     {
-        printf("mbedtls_ctr_drbg_seed failed\n");
-        return 1;
+        printf("Failed in mbedtls_ctr_drbg_seed: %d\n", ret);
+        return NOK;
     }
 
-    // Load certificates
-    ret = mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM,
-                                      MBEDTLS_SSL_PRESET_DEFAULT);
-    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
+    mbedtls_x509_crt cacert;
+    mbedtls_x509_crt clicert;
+    mbedtls_pk_context pkey;
 
-    // Set up SSL
-    if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0)
+    mbedtls_x509_crt_init(&cacert);
+    mbedtls_x509_crt_init(&clicert);
+    mbedtls_pk_init(&pkey);
+
+    // Load the trusted CA
+    ret = mbedtls_x509_crt_parse_file(&cacert, "../../role_based_cert/ca.crt");
+    if (ret != 0)
     {
-        printf("mbedtls_ssl_setup failed\n");
-        return 1;
+        printf("Failed to load CA certificate\n");
+        return NOK;
     }
 
-    // Connect to server
-    if ((ret = mbedtls_net_connect(&server_fd, "127.0.0.1", "4433", MBEDTLS_NET_PROTO_TCP)) != 0)
+    if ((ret = mbedtls_x509_crt_parse_file(&clicert, "../../role_based_cert/client.crt")) != 0)
     {
-        printf("mbedtls_net_connect failed\n");
-        return 1;
+        printf("Failed loading client cert\n");
+        return NOK;
+    }
+    if ((ret = mbedtls_pk_parse_keyfile(&pkey, "../../role_based_cert/client.key", NULL)) != 0)
+    {
+        printf("Failed loading key\n");
+        return NOK;
     }
 
-    mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
-
-    // Handshake
-    while ((ret = mbedtls_ssl_handshake(&ssl)) != 0)
+    if ((ret = mbedtls_ssl_config_defaults(&mbed_ssl_conf_, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM,
+                                           MBEDTLS_SSL_PRESET_DEFAULT)) != 0)
     {
-        if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE)
-        {
-            printf("mbedtls_ssl_handshake failed\n");
-            return 1;
-        }
+        printf("Failed in mbedtls_ssl_config_defaults: %d\n", ret);
+        return NOK;
     }
 
-    printf("TLS handshake succeeded\n");
+    mbedtls_ssl_conf_authmode(&mbed_ssl_conf_, MBEDTLS_SSL_VERIFY_NONE);
+    mbedtls_ssl_conf_ca_chain(&mbed_ssl_conf_, &cacert, NULL);
+    mbedtls_ssl_conf_own_cert(&mbed_ssl_conf_, &clicert, &pkey);
 
-    // Cleanup
-    mbedtls_net_free(&server_fd);
-    mbedtls_ssl_free(&ssl);
-    mbedtls_ssl_config_free(&conf);
-    mbedtls_ctr_drbg_free(&ctr_drbg);
-    mbedtls_entropy_free(&entropy);
 
-    return 0;
+
+    // mbedtls_net_connect(&server_fd, "192.168.10.10", "802", MBEDTLS_NET_PROTO_TCP);
+    server_fd.fd = sock;
+
+    mbedtls_ssl_conf_rng(&mbed_ssl_conf_, mbedtls_ctr_drbg_random, &mbed_ssl_ctr_drbg_);
+
+    char err_buf[100];
+    mbedtls_ssl_context mbed_ssl_contxt_;
+    mbedtls_ssl_init(&mbed_ssl_contxt_);
+    // Create a mbedtls_fd structure
+    mbedtls_ssl_init(&mbed_ssl_contxt_);
+    mbedtls_ssl_config *conf_ = &mbed_ssl_conf_;
+    if ((mbedtls_ssl_setup(&mbed_ssl_contxt_, conf_)) != 0)
+    {
+        printf("Failed in mbedtls_ssl_setup\n");
+        return NOK;
+    }
+
+    mbedtls_ssl_set_bio(&mbed_ssl_contxt_, &server_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
+
+    // Perform the SSL handshake
+    int i = 0;
+    int ssl_connect_ret = mbedtls_ssl_handshake(&mbed_ssl_contxt_);
+    while (ssl_connect_ret != 0 && i < 5)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+        mbedtls_strerror(ssl_connect_ret, err_buf, 100);
+        ssl_connect_ret = mbedtls_ssl_handshake(&mbed_ssl_contxt_);
+        i++;
+    }
+    if (i == 5)
+    {
+        printf("tried 5 times, exit now\n");
+        return NOK; // exit after 5 times connect tries
+    }
+
+    const char *hello = "Hello\n\n\n\n\n\n\n";
+    ret = mbedtls_ssl_write(&mbed_ssl_contxt_, (const unsigned char *)hello, strlen(hello));
+    if (ret <= 0)
+    {
+        printf("write error\n");
+    }
 }
